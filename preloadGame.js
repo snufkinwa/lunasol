5ccfac8ba4e8533fcf9f837d7c9e3ddd44e8c0ed
Janay Harris <jha2972@wgu.edu>
2024-10-08 22:42:17 -0500

    ...

--- preloadGame.js
+++ preloadGame.js
@@ -12,23 +12,23 @@ class preloadGame extends Phaser.Scene {
       console.error(`Failed to load: ${file.src}`);
     });
 
-    this.load.spritesheet("arrowLeft", "../assets/ARROWLEFT.png", {
+    this.load.spritesheet("arrowLeft", "assets/ARROWLEFT.png", {
       frameWidth: 16,
       frameHeight: 16,
     });
-    this.load.spritesheet("arrowRight", "../assets/ARROWRIGHT.png", {
+    this.load.spritesheet("arrowRight", "assets/ARROWRIGHT.png", {
       frameWidth: 16,
       frameHeight: 16,
     });
-    this.load.spritesheet("shift", "../assets/SHIFT.png", {
+    this.load.spritesheet("shift", "assets/SHIFT.png", {
       frameWidth: 42,
       frameHeight: 16,
     });
 
-    this.load.image("stars", "../assets/stars.png");
-    this.load.image("earth", "../assets/earth.png");
-    this.load.image("moon_surface", "../assets/moon_surface.png");
-    this.load.spritesheet("astronaut", "../assets/Astronaut_10x9.png", {
+    this.load.image("stars", "./assets/stars.png");
+    this.load.image("earth", "./assets/earth.png");
+    this.load.image("moon_surface", "./assets/moon_surface.png");
+    this.load.spritesheet("astronaut", "./assets/Astronaut_10x9.png", {
       frameWidth: 50,
       frameHeight: 50,
     });
